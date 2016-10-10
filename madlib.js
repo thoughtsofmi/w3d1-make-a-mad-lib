7b6ad7efd0857eb8bd2cc07d4094b03906c3f175
Young Mi Kim <ymkim23@gmail.com>
2016-10-10 18:05:54 -0400

    changes

--- madlib.js
+++ madlib.js
@@ -16,30 +16,10 @@ var ingVerb2 = "crying"
 var noun2 = "house"
 var pastTenseVerb = "loved"
 var pluralNoun2 = "children"
-var noun3 = "train"
 var adjective5 ="annoyed"
+var noun3 = "train"
 
-var sentence = `One day ${name1} and ${name2} decided to take a trip to the beach.`
-console.log(sentence)
-var sentence = `${name1} packed a bag full of ${adjective} ${food} and a big jug of ${liquid}.`
-console.log(sentence)
-var sentence = `${name2} bought a towel that had a pictue of a ${adjective2} ${animal} on it.`
-console.log(sentence)
-var sentence = `The beach was crowded but there were a few ${adjective3} spots left.`
-console.log(sentence)
-var sentence = `${name1} and ${name2} took a lap around ${adjective4} beach.
-console.log(sentence)`
-var sentence = `${name1} and ${name2} wanted to learn how to ${verb} so they rented a pair of ${pluralNoun} and hopped into the water.`
-console.log(sentence)
-var sentence = `Soon it was time for more snacks so ${name1} went to a stand to buy fish ${pluralFood}.`
-console.log(sentence)
-var sentence = `A lifeguard blew on his ${noun} because someone was ${ingVerb} too far into the ocean.`
-console.log(sentence)
-var sentence = `The sun was ${ingVerb2} so ${name1} and ${name2} rubbed ${noun2} on each other for protection.`
-console.log(sentence)
-var sentence = `Eventually the tide came too close and ${pastTenseVerb} all the ${pluralNoun2} away from the beach.`
-console.log(sentence)
-var sentence = `This really ${adjective5} them.`
-console.log(sentence)
-var sentence = `${name1} and ${name2} packed their ${noun3} and went home.`
+
+
+var sentence = `One day ${name1} and ${name2} decided to take a trip to the beach. ${name1} packed a bag full of ${adjective} ${food} and a big jug of ${liquid}.  ${name2} bought a towel that had a pictue of a ${adjective2} ${animal} on it.  The beach was crowded but there were a few ${adjective3} spots left. ${name1} and ${name2} took a lap around ${adjective4} beach.  ${name1} and ${name2} wanted to learn how to ${verb} so they rented a pair of ${pluralNoun} and hopped into the water.  Soon it was time for more snacks so ${name1} went to a stand to buy fish ${pluralFood}. A lifeguard blew on his ${noun} because someone was ${ingVerb} too far into the ocean.  The sun was ${ingVerb2} so ${name1} and ${name2} rubbed ${noun2} on each other for protection. Eventually the tide came too close and ${pastTenseVerb} all the ${pluralNoun2} away from the beach.  This really ${adjective5} them.  ${name1} and ${name2} packed their ${noun3} and went home.`
 console.log(sentence)
